30c08d308d18f43665709b4fff33a90498372a3b
KmylaSales <kammylas_2005@hotmail.com>
2022-05-29 17:34:36 -0300

    pesquisa paginada atualizada

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Router } from "express";
 import UserController from "./app/controllers/UserController";
 import ProductController from "./app/controllers/ProductController";
 import WishlistController from "./app/controllers/WishlistController";
+import WishProdController from "./app/controllers/WishProdController";
 
 const routes = new Router();
 
@@ -11,11 +12,11 @@ const routes = new Router();
 // //-----------------------------------------------------------------------------
 routes.post("/users", UserController.store);
 routes.put("/users/:req_email", UserController.update);
-routes.delete("/users/:req_id", UserController.delete);
+routes.delete("/users/:id", UserController.delete);
 routes.get("/users/:id", UserController.index);
 routes.get("/users/email/:req_email", UserController.findEmail);
 routes.get("/users/name/all", UserController.findAll);
-routes.get("/users/all", UserController.findAllUser);
+routes.get("/users/name/searchp", UserController.SearchAllC);
 
 // // Rotas Produtos
 // //-----------------------------------------------------------------------------
@@ -24,16 +25,21 @@ routes.put("/product/:req_id", ProductController.update);
 routes.delete("/product/delete/:req_id", ProductController.delete);
 routes.get("/product/search/:req_id", ProductController.index);
 routes.get("/product/searchAll", ProductController.findAll);
+routes.get("/product/name/searchp", ProductController.SearchAllP);
 routes.get("/product/searchWishlist/:req_id", ProductController.findWishlist);
 
 // // Rotas Wishlist
 // //-----------------------------------------------------------------------------
-routes.post("/wishlistProduct/:user_id", WishlistController.store);
-routes.put("/wishlist/:req_id/:product_id", WishlistController.update);
+routes.post("/wishlist/:user_id", WishlistController.store);
+// routes.put("/wishlist/", WishlistController.update);
 // routes.delete("/wishlist/", WishlistController.delete);
-// routes.get(
-//   "/wishlist/wishlist_product/:wishlist_id",
-//   WishlistController.findAllWishlist
-// );
+routes.get(
+  "/wishlist/wishlist_product/:wishlist_id",
+  WishlistController.findAllWishlist
+);
 
+// // Rotas Wishlist-Product
+// //-----------------------------------------------------------------------------
+routes.post("/wishlistProduct/:wishlist_id", WishProdController.store);
 export default routes;
+
